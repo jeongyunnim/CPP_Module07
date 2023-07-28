308d581f02a626da5ad280a58f76b4f503af5608
jeongyunnim <jeongyun7521@gmail.com>
2023-07-28 21:02:00 +0900

    feat: ex00 구현
    swap, min, max 프로토타입 맞추기.
    메인문 작성하기.

---
+++ ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "whatever.hpp"
+
+int main(void)
+{
+	const int	firstValueInt = 0;
+	const int	secondValueInt = 1;
+	// float	firstValueFloat;
+	// float	secondValueFloat;
+	// double	firstValueFloat;
+	// double	secondValueFloat;
+
+	min(firstValueInt, secondValueInt);
+	return (0);
+}
---
+++ ex00/whatever.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef _WHATEVER_HPP_
+#define _WHATEVER_HPP_
+
+template <typename T>
+void    swap(T& t1, T& t2)
+{
+	T *temp = &t1;
+
+	&t1 = &t2;
+	&t2 = temp;
+}
+
+template <typename T>
+T&    min(T& t1,T& t2)
+{
+	if (t1 < t2)
+		return (t1);
+	else
+		return (t2);
+}
+
+template <typename T>
+T&    min(const T& t1,const T& t2)
+{
+	if (t1 < t2)
+		return (t1);
+	else
+		return (t2);
+}
+
+template <typename T>
+T&    max(const T& t1,const T& t2)
+{
+	if (t1 > t2)
+		return (t1);
+	else
+		return (t2);
+}
+
+#endif
