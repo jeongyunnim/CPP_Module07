64ec28a51332f7a8f246d174f938a60f45fd41a7
jeongyunnim <jeongyun7521@gmail.com>
2023-07-29 16:41:40 +0900

    feat: 각 자료형 별로 test문 완성
    int 자료형으로 min/max 값에서 두 번째 인자가 출력되는지 테스트도 완료.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,14 +1,74 @@
 #include "whatever.hpp"
+#include "../Colors.hpp"
 
 int main(void)
 {
-	const int	firstValueInt = 0;
-	const int	secondValueInt = 1;
-	// float	firstValueFloat;
-	// float	secondValueFloat;
-	// double	firstValueFloat;
-	// double	secondValueFloat;
+	{
+		std::cout << Colors::BoldWhiteString("\n-----------------<<int type test>>-----------------\n") << std::endl;
+		int			intZero = 0;
+		int			intMax = __INT32_MAX__;
+		std::cout << Colors::CyanString("[original value]") << "\nzero: " << intZero << "\nmax: " << intMax << std::endl;
+		swap(intZero, intMax);
+		std::cout << Colors::CyanString("[swapped value]") << "\nzero: " << intZero << "\nmax: " << intMax << std::endl;
+		std::cout << Colors::CyanString("[min result]") << "\n-> "<< min(intZero, intMax) << std::endl;
+		std::cout << Colors::CyanString("[max result]") << "\n-> "<< max(intZero, intMax) << std::endl;
+	}
+	{
+		std::cout << Colors::BoldWhiteString("\n-----------------<<const int type test>>-----------------\n") << std::endl;
+		const int	constIntZero = 0;
+		const int	constIntMax = __INT32_MAX__;
+		std::cout << Colors::CyanString("[original value]") << "\nzero: " << constIntZero << "\nmax: " << constIntMax << std::endl;
+		std::cout << Colors::CyanString("[min result]") << "\n-> "<< min(constIntZero, constIntMax) << std::endl;
+		std::cout << Colors::CyanString("[max result]") << "\n-> "<< max(constIntZero, constIntMax) << std::endl;
+	}
+	{
+		std::cout << Colors::BoldWhiteString("\n-----------------<<float type test>>-----------------\n") << std::endl;
+		float		floatZero = 0;
+		float		floatInf = std::numeric_limits<float>::infinity();
 
-	min(firstValueInt, secondValueInt);
+		std::cout << Colors::CyanString("[original value]") << "\nzero: " << floatZero << "\nmax: " << floatInf << std::endl;
+		swap(floatZero, floatInf);
+		std::cout << Colors::CyanString("[swapped value]") << "\nzero: " << floatZero << "\nmax: " << floatInf << std::endl;
+		std::cout << Colors::CyanString("[min result]") << "\n-> "<< min(floatZero, floatInf) << std::endl;
+		std::cout << Colors::CyanString("[max result]") << "\n-> "<< max(floatZero, floatInf) << std::endl;
+	}
+	{
+		std::cout << Colors::BoldWhiteString("\n-----------------<<const float type test>>-----------------\n") << std::endl;
+		const float	constFloatZero = 0;
+		const float	constFloatInf = std::numeric_limits<float>::infinity();
+
+		std::cout << Colors::CyanString("[original value]") << "\nzero: " << constFloatZero << "\nmax: " << constFloatInf << std::endl;
+		std::cout << Colors::CyanString("[min result]") << "\n-> "<< min(constFloatZero, constFloatInf) << std::endl;
+		std::cout << Colors::CyanString("[max result]") << "\n-> "<< max(constFloatZero, constFloatInf) << std::endl;
+	}
+	{
+		std::cout << Colors::BoldWhiteString("\n-----------------<<double type test>>-----------------\n") << std::endl;
+		float		DoubleZero = 0;
+		float		DoubleInf = std::numeric_limits<double>::infinity();
+
+		std::cout << Colors::CyanString("[original value]") << "\nzero: " << DoubleZero << "\nmax: " << DoubleInf << std::endl;
+		swap(DoubleZero, DoubleInf);
+		std::cout << Colors::CyanString("[swapped value]") << "\nzero: " << DoubleZero << "\nmax: " << DoubleInf << std::endl;
+		std::cout << Colors::CyanString("[min result]") << "\n-> "<< min(DoubleZero, DoubleInf) << std::endl;
+		std::cout << Colors::CyanString("[max result]") << "\n-> "<< max(DoubleZero, DoubleInf) << std::endl;
+	}
+	{
+		std::cout << Colors::BoldWhiteString("\n-----------------<<const double type test>>-----------------\n") << std::endl;
+		const float	constDoubleZero = 0;
+		const float	constDoubleInf = std::numeric_limits<double>::infinity();
+
+		std::cout << Colors::CyanString("[original value]") << "\nzero: " << constDoubleZero << "\nmax: " << constDoubleInf << std::endl;
+		std::cout << Colors::CyanString("[min result]") << "\n-> "<< min(constDoubleZero, constDoubleInf) << std::endl;
+		std::cout << Colors::CyanString("[max result]") << "\n-> "<< max(constDoubleZero, constDoubleInf) << std::endl;
+	}
+	{
+		std::cout << Colors::BoldWhiteString("\n-----------------<<min/max equal value test>>-----------------\n") << std::endl;
+
+		int	intMax = INT32_MAX;
+		int	intMaxCopy = intMax;
+		std::cout << Colors::CyanString("[original value's pointer]") << "\nMax: " << &intMax << "\nmaxCopy: " << &intMaxCopy << std::endl;
+		std::cout << Colors::CyanString("[min result]") << "\n-> "<< &min(intMax, intMaxCopy) << std::endl;
+		std::cout << Colors::CyanString("[max result]") << "\n-> "<< &max(intMax, intMaxCopy) << std::endl;
+	}
 	return (0);
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -5,10 +5,10 @@
 template <typename T>
 void    swap(T& t1, T& t2)
 {
-	T *temp = &t1;
+	T temp = t1;
 
-	&t1 = &t2;
-	&t2 = temp;
+	t1 = t2;
+	t2 = temp;
 }
 
 template <typename T>
@@ -21,7 +21,7 @@ T&    min(T& t1,T& t2)
 }
 
 template <typename T>
-T&    min(const T& t1,const T& t2)
+const T&    min(const T& t1,const T& t2)
 {
 	if (t1 < t2)
 		return (t1);
@@ -30,7 +30,16 @@ T&    min(const T& t1,const T& t2)
 }
 
 template <typename T>
-T&    max(const T& t1,const T& t2)
+T&    max(T& t1,T& t2)
+{
+	if (t1 > t2)
+		return (t1);
+	else
+		return (t2);
+}
+
+template <typename T>
+const T&    max(const T& t1,const T& t2)
 {
 	if (t1 > t2)
 		return (t1);
